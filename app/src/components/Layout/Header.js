47c250b494eaeaff18c18193d96b2b6c4332ad83
mukesh jamod <mukeshjamod28@gmail.com>
2023-04-14 12:15:34 +0530

    fix : seachicon  and search box size change

--- app/src/components/Layout/Header.js
+++ app/src/components/Layout/Header.js
@@ -1,55 +1,58 @@
 import { Link } from 'react-router-dom';
 import logo from '../images/logo.png';
 import classes from './Header.module.css';
-import { FaUser,FaSearch, FaShoppingCart } from 'react-icons/fa';
+import { FaUser, FaSearch, FaShoppingCart } from 'react-icons/fa';
 import { useStateValue } from '../../store/CartProvider';
+import { MdArrowDropDown } from 'react-icons/md';
+import Dropdown from './Dropdown';
 
 
 function Header() {
-  const [{ cart }, dispatch] = useStateValue();
-  // const Navigate = useNavigate();
+  const [{ cart }] = useStateValue();
 
-  console.log("my cart", cart);
+  const options = [
+  {value:'green', label:'green'},
+  {value:'blue',  label:" blue"}
+  ]
 
-  // function cartIconHandler (){
-  //   Navigate('/checkout');
-  // }
+
 
   return (
-    <nav className={classes.header}>
-      <div className={classes.logo}>
-        <img src={logo} width={90} height={77} alt="logo" />
-      </div>
+    <nav className={classes.header}><Link to='/'><div className={classes.logo}>
+      <img src={logo} width={90} height={77} alt="logo" />
+    </div>
+    </Link>
+
       <div className={classes.search}>
+        <div className={classes.dropdown}>
+          <span>
+            All
+          </span>
+          <MdArrowDropDown size={20} />
+          <Dropdown placeholder="Select ..." options={options}/>
+
+        </div>
         <input type="text" placeholder="Search Seabasket.in" />
-        {/* <button type="submit">Search</button> */}
-       <div className={classes.searchicon}><FaSearch/> </div> 
+
+        <div className={classes.searchIcon}><FaSearch size={25} /></div>
       </div>
 
-
-
-      <Link to='/profile'className={classes.userIcon}><FaUser /></Link>
+      <Link to='/profile' className={classes.userIcon}><FaUser size={25} /></Link>
 
       <Link to='/login' className={classes.link}>
         <div className={classes.option}>
-          <span>Hello,user</span>
-          <span>
-            sign in
-          </span>
+          <span>Hello, Sing in</span>
+
         </div>
       </Link>
       <Link to='/checkout'>
         <div className={classes.icon}>
-          <FaShoppingCart />
+          <FaShoppingCart size={25} />
           <span className={classes.count}>{cart.length}</span>
         </div>
-
       </Link>
-
     </nav>
   );
 }
 
 export default Header;
-
-
