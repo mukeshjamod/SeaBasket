3cfc343422855e7b5af4e5a507ee80e6559e0bdf
mukesh jamod <mukeshjamod28@gmail.com>
2023-04-20 23:24:12 +0530

    feat: added onClick handler

--- app/src/pages/Footer.js
+++ app/src/pages/Footer.js
@@ -6,12 +6,19 @@ import classes from './Footer.module.css';
 import FooterPage from '../modal/Footer';
 import FooterEndPage from '../modal/FooterEndPage';
 function Footer() {
+
+  const backhandler = (event) =>{
+event.preventDefault();
+
+
+document.documentElement.scrollTop = 0;
+  }
   return (
     // <FooterPage></FooterPage>
     <div className={classes.footer}>
       
-      <div className={classes.top}>
-        <h1>Back to Top</h1>
+      <div className={classes.top} onClick={backhandler}>
+        <h1 >Back to Top</h1>
       </div>
       <div className={classes.down}> 
         <div>
