323099ebe491fc2d0007406a29bb130f539780eb
mukesh jamod <mukeshjamod28@gmail.com>
2023-04-20 23:28:15 +0530

    feat: added dropdown list in header

--- app/src/components/Layout/Header.js
+++ app/src/components/Layout/Header.js
@@ -1,38 +1,43 @@
 import { Link } from 'react-router-dom';
 import logo from '../images/logo.png';
 import classes from './Header.module.css';
+import MyDropdownButton from './MyDropdownButton';
 import { FaUser, FaSearch, FaShoppingCart } from 'react-icons/fa';
 import { useStateValue } from '../../store/CartProvider';
-import { MdArrowDropDown } from 'react-icons/md';
-import Dropdown from './Dropdown';
+// import BasicButton from './DropDown';
+
+  // import { MdArrowDropDown } from 'react-icons/md';
+// import Dropdown from './Dropdown';
 
 
 function Header() {
   const [{ cart }] = useStateValue();
 
-  const options = [
-  {value:'green', label:'green'},
-  {value:'blue',  label:" blue"}
-  ]
+  // const options = [
+  // {value:'green', label:'green'},
+  // {value:'blue',  label:" blue"},
+  // {value:'slow', label:'slow'}
+  // ]
 
 
 
   return (
-    <nav className={classes.header}><Link to='/'><div className={classes.logo}>
+    <nav className={classes.header}>
+      <Link to='/'><div className={classes.logo}>
       <img src={logo} width={90} height={77} alt="logo" />
     </div>
     </Link>
 
       <div className={classes.search}>
-        <div className={classes.dropdown}>
-          <span>
-            All
-          </span>
-          <MdArrowDropDown size={20} />
-          <Dropdown placeholder="Select ..." options={options}/>
+        <div className={classes.dropdown}> 
+         
+        
+         <MyDropdownButton/>
+           {/* <MdArrowDropDown size={20} /> */}
+          {/* <Dropdown options={options}/> */}
 
         </div>
-        <input type="text" placeholder="Search Seabasket.in" />
+        <input style={{fontSize:14, paddingLeft:10,}} type="text" placeholder="Search Seabasket.in" />
 
         <div className={classes.searchIcon}><FaSearch size={25} /></div>
       </div>
